0de50ccead6799330e928d55ef8bd81b1e6bb82f
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-05-15 22:21:53 +0200

    signout error en cookie, sigue sin cogerla

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const authController = require ('../controller/auth');
+const {authorizers} = require('../middelwares');
 
 module.exports = (db) => {
     router.post ('/signup', authController.signup(db))
     router.post ('/signin', authController.signin(db))
-    router.post ('/singout', authController.signout())
+    router.post ('/singout',authorizers, authController.signout())
     
     return router
 };
