29255e5e7fae2eecedebf50d391ec9d1b4e13434
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-05-15 23:06:50 +0200

    archivo obj renombrado

--- ObjRestCountr.js
+++
@@ -1,8 +0,0 @@
-const obj = require ('./pruebarefactorizar.json');
-
-console.log(obj)
-
-const name = obj.map(nombre => nombre.name.common)
-// console.log(name)
-const currencies  = obj.map(moneda => moneda.currencies.EUR.name)
-//console.log(currencies)
